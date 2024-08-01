fc3e441385cc5fd37e6836909157a7a69c15c1aa
Salar Al Ali <ooralas@outlook.com>
2024-08-01 04:53:04 +0200

    feature/add-swagger-postman-integration

--- README.md
+++ README.md
@@ -25,10 +25,59 @@ To start the development environment, run the following command in the project r
 docker-compose -f docker-compose.yml up --build
 ```
 
-### Production Setup
+### Production Setup (will come later)
 
 To start the production environment, run the following command in the project root:
 
 ```shell
 docker-compose -f docker-compose.prod.yml up --build
 ```
+
+### Environment Variables
+
+The project relies on environment variables to configure various aspects of the application. An `.env.example` file is provided in the project root as a template for the environment configuration.
+
+To set up the environment variables:
+
+- Create a copy of the `.env.example` file and rename it to `.env`:
+
+  ```shell
+  cp .env.example .env
+  ```
+
+- Open the `.env` file and update the variables as needed:
+
+  ```yml
+  NODE_ENV=development
+
+  API_PROTOCOL=http
+  API_HOST=localhost
+  API_PORT=3000
+
+  DB_URL=postgresql://<DB_USER>:<DB_PASSWORD>@<DB_HOST>:<DB_PORT><DB_NAME>
+
+  ALIAS_LENGTH=5
+  ```
+
+This file defines the protocol, host, and port for the API server, among other configuration settings.
+
+## API Documentation
+
+The API documentation is generated using the Swagger module of NestJS and is saved in the `api-documentation/swagger-document.json` file. This JSON file follows the OpenAPI standard and can be imported directly into Postman for testing and debugging the endpoints of the application.
+
+The Swagger UI is also accessible at `/swagger` for interacting with the API documentation through a web interface.
+
+By importing this documentation into Postman, all the endpoints of the application will be accessible, allowing for easy testing and debugging of the API.
+
+The `swagger-document.json` file is dynamically updated whenever changes are made to the existing controllers (endpoints) or new endpoints are added, as long as the appropriate Swagger decorators are used. This ensures that the API documentation is always up-to-date with the current state of the application's endpoints.
+
+### Using the API Documentation in Postman
+
+The API documentation can be easily imported into Postman for testing and debugging purposes. Follow these steps:
+
+1. Open Postman
+2. Click on "Import" in the top left corner
+3. Select `swagger-document.json` file from project folder
+4. Click "Continue" and then "Import"
+
+For detailed instructions on importing Swagger/OpenAPI specifications into Postman, refer to the [official Postman documentation](https://learning.postman.com/docs/getting-started/importing-and-exporting/importing-from-swagger/).
--- src/links/link.controller.ts
+++ src/links/link.controller.ts
@@ -15,7 +15,16 @@ import { CreateLinkDto } from './dto/create-link.dto';
 import { UpdateLinkDto } from './dto/update-link.dto';
 
 import { LinkAnalyticService } from 'src/link-analytic/link-analytic.service';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiParam,
+} from '@nestjs/swagger';
+import { Link } from './entities/link.entity';
 
+@ApiTags('links')
 @Controller('links')
 export class LinkController {
   constructor(
@@ -25,18 +34,33 @@ export class LinkController {
   ) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new link' })
+  @ApiBody({ type: CreateLinkDto })
+  @ApiResponse({
+    status: 201,
+    description: 'The link has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Invalid input data.' })
   create(@Body() createLinkDto: CreateLinkDto) {
     return this.linksService.create(createLinkDto);
   }
 
   @Get()
-  async findAll(@Req() request: Request) {
-    console.log(await this.linkAnalyticService.findAll());
-
+  @ApiOperation({ summary: 'Retrieve all links' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved links.',
+    type: [Link],
+  })
+  async findAll() {
     return await this.linksService.findAll();
   }
 
   @Get(':alias')
+  @ApiOperation({ summary: 'Retrieve a link by alias' })
+  @ApiParam({ name: 'alias', required: true, type: String })
+  @ApiResponse({ status: 200, description: 'Successfully retrieved link.' })
+  @ApiResponse({ status: 404, description: 'Link not found.' })
   async findOne(@Req() request: Request, @Param('alias') alias: string) {
     const link = await this.linksService.findOneByAliasAndUpdateViews(alias);
 
@@ -51,11 +75,26 @@ export class LinkController {
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a link' })
+  @ApiParam({ name: 'id', required: true, type: String })
+  @ApiBody({ type: UpdateLinkDto })
+  @ApiResponse({
+    status: 200,
+    description: 'The link has been successfully updated.',
+  })
+  @ApiResponse({ status: 404, description: 'Link not found.' })
   async update(@Param('id') id: string, @Body() updateLinkDto: UpdateLinkDto) {
-    return await this.linksService.updateLink(+id, updateLinkDto);
+    await this.linksService.updateLink(+id, updateLinkDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a link' })
+  @ApiParam({ name: 'id', required: true, type: String })
+  @ApiResponse({
+    status: 200,
+    description: 'The link has been successfully deleted.',
+  })
+  @ApiResponse({ status: 404, description: 'Link not found.' })
   remove(@Param('id') id: string) {
     return this.linksService.remove(+id);
   }
